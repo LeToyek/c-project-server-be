00218353b5b3d5a6c578b2c873265dc4a70492c1
Maulana Arif Wijaya <maulanatoyek@gmail.com>
2024-01-25 23:52:35 +0700

    feat: init socket io

--- src/index.ts
+++ src/index.ts
@@ -6,24 +6,36 @@ import { UserHandlerImpl } from "./pkg/handler/userHandler";
 import { UserRouters } from "./routers/userRouters";
 import { Sequelize } from "sequelize";
 import { initUser } from "./pkg/repository/db/models/user";
+// import { WebSocket, WebSocketServer } from "ws";
+const { Server } = require("socket.io");
+const { join } = require("node:path");
 require("dotenv").config();
 
 const app = express();
+const server = require("http").createServer(app);
+const io = new Server(server);
 
 app.use(express.json());
 app.use(cors());
 
-app.get("/", async (req, res) => {
-  try {
-    return res.json({ message: "Hello World" });
-  } catch (error) {
-    return res.json({ error: error });
-  }
+app.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "index.html"));
 });
 
-app.post("/", (req, res: Response) => {
-  const { name, email } = req.body;
-  return res.json({ name, email });
+io.on("connection", (socket:any) => {
+  console.log("a user connected");
+  
+  socket.on("chat message", (msg:string) => {
+    console.log("message: " + msg);
+    io.emit("chat message", msg);
+  });
+
+  socket.on("hello",(msg:string)=>{
+    console.log("message: " + msg);
+  })
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
 });
 
 const start = async () => {
@@ -39,21 +51,22 @@ const start = async () => {
       }
     );
     await sequelize.authenticate();
-    initUser(sequelize)
+    initUser(sequelize);
     await sequelize.sync();
-    
-    const userRepository = new UserRepositoryImpl(sequelize)
-    const userHandler = new UserHandlerImpl(userRepository)
-    const userRouter = new UserRouters(userHandler)
-    userRouter.registerRouters("/users", app)
 
-    app.listen(process.env.PORT, () => {
+    const userRepository = new UserRepositoryImpl(sequelize);
+    const userHandler = new UserHandlerImpl(userRepository);
+    const userRouter = new UserRouters(userHandler);
+    userRouter.registerRouters("/users", app);
+
+    server.listen(process.env.PORT, () => {
       console.log(`Server running on port ${process.env.PORT}`);
     });
+    
   } catch (error) {
     console.log(error);
     process.exit(1);
   }
-}
+};
 
-start()
+start();
